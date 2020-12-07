935eff29640abccc3f691d12b8f055fe9805582a
Michal Opala <sk4zuzu@gmail.com>
2020-12-07 09:40:36 +0100

    k8s-engines-on-prem/kubespray: initial

---
+++ k8s-engines-on-prem/kubespray/docs/au1.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+: ${RECORDING:=$1}
+
+set -o errexit -o nounset -o pipefail
+
+if [[ "$RECORDING" != true ]]; then
+    which realpath xargs dirname asciinema time make kubectl
+    exec asciinema rec --overwrite $0.cast -c "$0 true"
+fi
+
+SELF=$(realpath $0 | xargs dirname) && cd $SELF/../
+
+time make au1
+
+time make au1upgrade
