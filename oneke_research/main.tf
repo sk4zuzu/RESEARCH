d4e4b4da530bb5a7c8f6d0f17898adfbef8ae9fb
Michal Opala <sk4zuzu@gmail.com>
2023-12-22 16:33:14 +0100

    oneke_research: update (ether AR)

--- oneke_research/main.tf
+++ oneke_research/main.tf
@@ -17,32 +17,50 @@ provider "opennebula" {
 locals {
   nics = {
     eth0 = {
-      network_id  = data.opennebula_virtual_network.oneke["service"].id
-      floating_ip = true
+      network_id  = opennebula_virtual_network.ethernet.id
+      floating_ip = false
     }
     eth1 = {
-      network_id  = opennebula_virtual_network.oneke["reservation"].id
+      network_id  = data.opennebula_virtual_network.service.id
+      floating_ip = true
+    }
+    eth2 = {
+      network_id  = opennebula_virtual_network.reservation.id
       floating_ip = true
     }
   }
 }
 
-data "opennebula_virtual_network" "oneke" {
-  for_each = toset(["service", "private"])
-  name     = each.key
+resource "opennebula_virtual_network" "ethernet" {
+  name = "ethernet"
+  ar {
+    ar_type = "ETHER"
+    size    = 16
+  }
 }
 
-resource "opennebula_virtual_network" "oneke" {
-  for_each = toset(["reservation"])
-  name     = each.key
+data "opennebula_virtual_network" "service" {
+  name = "service"
+}
 
-  reservation_vnet    = data.opennebula_virtual_network.oneke["private"].id
-  reservation_size    = 20
-  reservation_ar_id   = 0
-  reservation_auto_gw = true
+data "opennebula_virtual_network" "private" {
+  name = "private"
+}
+
+resource "opennebula_virtual_network" "reservation" {
+  name                 = "reservation"
+  reservation_vnet     = data.opennebula_virtual_network.private.id
+  reservation_size     = 20
+  reservation_ar_id    = 0
+  reservation_first_ip = "172.20.0.123"
+  reservation_auto_gw  = true
 }
 
 module "vrouter" {
+  depends_on = [
+    opennebula_virtual_network.ethernet,
+    opennebula_virtual_network.reservation,
+  ]
   source    = "./modules/vrouter/"
   instances = 2
   nics      = local.nics
@@ -51,5 +69,5 @@ module "vrouter" {
 module "oneflow" {
   depends_on = [module.vrouter]
   source     = "./modules/oneflow/"
-  network_id = local.nics["eth1"].network_id
+  network_id = local.nics["eth2"].network_id
 }
--- oneke_research/modules/vrouter/main.tf
+++ oneke_research/modules/vrouter/main.tf
@@ -33,6 +33,14 @@ resource "opennebula_image" "oneke" {
   path         = each.value
 }
 
+locals {
+  start_script = <<-SHELL
+    #!/bin/sh
+    set -e
+    ip link set dev eth0 up
+  SHELL
+}
+
 resource "opennebula_virtual_router_instance_template" "oneke" {
   name        = "vr_oneke"
   permissions = "642"
@@ -49,20 +57,50 @@ resource "opennebula_virtual_router_instance_template" "oneke" {
     SSH_PUBLIC_KEY = "$USER[SSH_PUBLIC_KEY]"
     PASSWORD       = "asd"
 
-    ONEAPP_VNF_NAT4_ENABLED        = "YES"
-    ONEAPP_VNF_NAT4_INTERFACES_OUT = "eth0"
+    ONEAPP_VNF_KEEPALIVED_PASSWORD = "asd"
 
-    ONEAPP_VNF_KEEPALIVED_PASSWORD     = "asd"
+    ONEAPP_VROUTER_ETH0_VIP0 = "10.11.12.13"
+
+    # NAT4
+
+    ONEAPP_VNF_NAT4_ENABLED        = "YES"
+    ONEAPP_VNF_NAT4_INTERFACES_OUT = "eth1"
+
+    # HAPROXY
+
     ONEAPP_VNF_HAPROXY_ENABLED         = "YES"
     ONEAPP_VNF_HAPROXY_ONEGATE_ENABLED = "YES"
+    ONEAPP_VNF_HAPROXY_INTERFACES      = "!eth3"
 
-    ONEAPP_VNF_HAPROXY_LB0_IP   = "<ONEAPP_VROUTER_ETH0_VIP0>"
+    ONEAPP_VNF_HAPROXY_LB0_IP   = "<ONEAPP_VROUTER_ETH1_VIP0>"
     ONEAPP_VNF_HAPROXY_LB0_PORT = "5432"
 
-    ONEAPP_VNF_HAPROXY_LB1_IP           = "<ONEAPP_VROUTER_ETH0_VIP0>"
+    ONEAPP_VNF_HAPROXY_LB1_IP           = "<ONEAPP_VROUTER_ETH2_VIP0>"
     ONEAPP_VNF_HAPROXY_LB1_PORT         = "1234"
     ONEAPP_VNF_HAPROXY_LB1_SERVER0_HOST = "10.2.11.40"
     ONEAPP_VNF_HAPROXY_LB1_SERVER0_PORT = "9869"
+
+    # LVS
+
+    ONEAPP_VNF_LB_ENABLED         = "YES"
+    ONEAPP_VNF_LB_ONEGATE_ENABLED = "YES"
+    ONEAPP_VNF_LB_INTERFACES      = "!eth2"
+
+    ONEAPP_VNF_LB0_IP        = "<ONEAPP_VROUTER_ETH1_VIP0>"
+    ONEAPP_VNF_LB0_PORT      = "2345"
+    ONEAPP_VNF_LB0_PROTOCOL  = "TCP"
+    ONEAPP_VNF_LB0_METHOD    = "NAT"
+    ONEAPP_VNF_LB0_SCHEDULER = "rr"
+
+    ONEAPP_VNF_LB1_IP           = "<ONEAPP_VROUTER_ETH2_VIP0>"
+    ONEAPP_VNF_LB1_PORT         = "4321"
+    ONEAPP_VNF_LB1_PROTOCOL     = "TCP"
+    ONEAPP_VNF_LB1_METHOD       = "NAT"
+    ONEAPP_VNF_LB1_SCHEDULER    = "rr"
+    ONEAPP_VNF_LB1_SERVER0_HOST = "10.2.11.40"
+    ONEAPP_VNF_LB1_SERVER0_PORT = "9869"
+
+    START_SCRIPT_BASE64 = base64encode(local.start_script)
   }
 
   os {
@@ -121,4 +159,18 @@ resource "opennebula_virtual_router_nic" "eth1" {
 
   virtual_router_id = opennebula_virtual_router.oneke.id
   network_id        = var.nics.eth1.network_id
+}
+
+resource "opennebula_virtual_router_nic" "eth2" {
+  depends_on = [
+    opennebula_virtual_router_instance.oneke,
+    opennebula_virtual_router_nic.eth0,
+    opennebula_virtual_router_nic.eth1,
+  ]
+
+  model       = "virtio"
+  floating_ip = var.nics.eth2.floating_ip
+
+  virtual_router_id = opennebula_virtual_router.oneke.id
+  network_id        = var.nics.eth2.network_id
 }
